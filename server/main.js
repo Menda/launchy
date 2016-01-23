d1b6b325ff77956e64689f7e5bfc3d5c5e9993a4
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-01-23 17:14:37 +0100

    Populate districts on startup

--- server/main.js
+++ server/main.js
@@ -1,4 +1,31 @@
-  Meteor.startup(function () {
-      // code to run on server at startup
-  });
-  
+Meteor.startup(function () {
+  // Districts population
+  if (Districts.find().count() === 0) {
+    console.log('Populating Spanish districts');
+
+    var districtsSpain = [
+      'alava', 'albacete', 'alicante', 'almeria', 'asturias', 'avila',
+      'badajoz', 'barcelona', 'burgos',
+      'caceres', 'cadiz', 'cantabria', 'castellon', 'ceuta', 'ciudad_real', 'cordoba', 'cuenca',
+      'gerona', 'granada', 'guadalajara', 'guipuzcoa',
+      'huelva', 'huesca',
+      'islas_baleares',
+      'jaen',
+      'la_coruna', 'la_rioja', 'las_palmas', 'leon', 'lerida', 'lugo',
+      'madrid', 'malaga', 'melilla', 'murcia',
+      'navarra',
+      'orense',
+      'palencia', 'pontevedra',
+      'salamanca', 'santa_cruz_de_tenerife', 'segovia', 'sevilla', 'soria',
+      'tarragona', 'teruel', 'toledo',
+      'valencia', 'valladolid', 'vizcaya',
+      'zamora', 'zaragoza'
+    ]
+    var districtsSpainRoot = 'districts/spain/';
+    _.each(districtsSpain, function(districtName) {
+      var district = JSON.parse(
+        Assets.getText(districtsSpainRoot + districtName + '.json'));
+      Districts.insert(district);
+    });
+  }
+});
