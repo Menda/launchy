d5e0d663054ef6b13c7666c208aede4147f32459
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-02-17 17:51:58 +0100

    Tmp preparation

--- collections/stores/stores.js
+++ collections/stores/stores.js
@@ -1,5 +1,11 @@
 Stores = {};
 
+if (Meteor.settings.public.environment === 'development'|'staging') {
+
+} else {
+
+}
+
 // var imageStore = new FS.Store.S3('images', {
 //   accessKeyId: Meteor.settings.accessKeyId, //required
 //   secretAccessKey: Meteor.settings.secretAccessKey, //required
@@ -20,3 +26,45 @@ Stores.thumbs = new FS.Store.GridFS('thumbs', {
     // The new file size will be automatically detected and set for this store
   }
 });
+
+/* TODO
+if (Meteor.isServer) {
+  var imageStore = new FS.Store.S3("images", {
+    accessKeyId: Meteor.settings.AWSAccessKeyId, 
+    secretAccessKey: Meteor.settings.AWSSecretAccessKey, 
+    bucket: Meteor.settings.AWSBucket
+  });
+
+  Images = new FS.Collection("Images", {
+    stores: [imageStore],
+    filter: {
+      allow: {
+        contentTypes: ['image/*']
+      }
+    }
+  });
+}
+
+// On the client just create a generic FS Store as don't have
+// access (or want access) to S3 settings on client
+if (Meteor.isClient) {
+  var imageStore = new FS.Store.S3("images");
+  Images = new FS.Collection("Images", {
+    stores: [imageStore],
+    filter: {
+      allow: {
+        contentTypes: ['image/*']
+      },
+      onInvalid: function(message) {
+        toastr.error(message);
+      }
+    }
+  });
+}
+
+// Allow rules
+Images.allow({
+  insert: function() { return true; },
+  update: function() { return true; }
+});
+*/
