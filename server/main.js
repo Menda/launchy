3b1322438d070c08f2f991a00334b93a86994ac9
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-03-11 12:55:27 +0100

    Do not show debug errors for FS

--- server/main.js
+++ server/main.js
@@ -16,7 +16,7 @@ function checkEnvVars() {
 function setFSSettings() {
   console.log('Setting image upload and handling config');
   if (Meteor.settings.public.environment === 'development'|'staging') {
-    FS.debug = true; // enable CFS debug logging
+    FS.debug = false;
   }
 
   // default GET request headers
