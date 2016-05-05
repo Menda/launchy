c92a57c1e77bb0967a1d5c54327d8c6e606b866a
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-05-05 12:23:12 +0200

    Deny profile edition from shell

--- server/security.js
+++ server/security.js
@@ -2,9 +2,17 @@
 // are allowed from the client code to be executed. This does not apply to
 // Meteor.methods actions, as those are executed server-side.
 'use strict';
+import {Meteor} from 'meteor/meteor';
+
 import {Districts, Makes, Cars, Blogposts} from '/collections/collections.js';
 import {Images} from '/server/collections.js';
 
+
+Meteor.users.deny({
+  update: function() {
+    return true;
+  }
+});
 
 Districts.deny({
   insert: () => true,
